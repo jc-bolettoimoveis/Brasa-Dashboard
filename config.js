83667ff12a1f0383237c8407081716655fcbeb5a
jc-bolettoimoveis <joaoclaudio@bolettoimoveis.com.br>
2026-07-16 03:16:58 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
    ============================================================ */
 window.CONFIG = {
   // Nome exato do arquivo atualizado no diretório do seu GitHub
-  XLS_FILE: "BC Contagem 2026 (2).xlsx", 
+  XLS_FILE: "bc_contagem_2026.xlsx", 
   ANO: 2026,                      // ano a exibir
   LAT: -30.0331,                  // Porto Alegre (para buscar o clima)
   LON: -51.23
