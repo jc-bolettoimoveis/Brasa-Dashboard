ffa7f8f5a3ebef3c5c0cb83a0a99b120edcddd8c
jc-bolettoimoveis <joaoclaudio@bolettoimoveis.com.br>
2026-07-16 02:47:11 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
    CONFIGURAÇÃO DO PAINEL — Brasa Church
    ============================================================ */
 window.CONFIG = {
-  // Coloque o arquivo .xls na mesma pasta que este arquivo config.js
-  XLS_FILE: "BC Contagem 2026 (2).xlsx",          // <-- Nome do seu arquivo Excel no diretório
+  // Nome exato do arquivo atualizado no diretório do seu GitHub
+  XLS_FILE: "BC Contagem 2026 (2).xlsx", 
   ANO: 2026,                      // ano a exibir
   LAT: -30.0331,                  // Porto Alegre (para buscar o clima)
   LON: -51.23
